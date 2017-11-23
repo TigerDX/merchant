1155c2225822441e18139606bcc0b57e0cf34754
Benjamin Murden <ben.murden@gmail.com>
2017-11-23 12:46:04 +0900

    Fix import of jinja2 when not using jinja2 in Django 1.9

--- billing/templatetags/jinja2_tags.py
+++ billing/templatetags/jinja2_tags.py
@@ -1,28 +1,34 @@
 from django.template import Library
 from django.template.loader import render_to_string
-from jinja2 import nodes
-from jinja2.ext import Extension
 
+try:
+    from jinja2 import nodes
+    from jinja2.ext import Extension
+except ImportError:
+    nodes = None
+    Extension = None
 
 register = Library()
 
 
-class MerchantExtension(Extension):
+if Extension:
+    class MerchantExtension(Extension):
 
-    tags = set(['render_integration'])
+        tags = set(['render_integration'])
 
-    def parse(self, parser):
-        stream = parser.stream
-        lineno = stream.next().lineno
+        def parse(self, parser):
+            stream = parser.stream
+            lineno = stream.next().lineno
 
-        obj = parser.parse_expression()
-        call_node = self.call_method('render_integration', args=[obj])
+            obj = parser.parse_expression()
+            call_node = self.call_method('render_integration', args=[obj])
 
-        return nodes.Output([call_node]).set_lineno(lineno)
+            return nodes.Output([call_node]).set_lineno(lineno)
 
-    @classmethod
-    def render_integration(self, obj):
-        form_str = render_to_string(obj.template, {'integration': obj})
-        return form_str
+        @classmethod
+        def render_integration(self, obj):
+            form_str = render_to_string(obj.template, {'integration': obj})
+            return form_str
 
-register.tag(MerchantExtension)
+
+    register.tag(MerchantExtension)
